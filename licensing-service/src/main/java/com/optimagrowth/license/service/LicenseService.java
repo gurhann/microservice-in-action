03cc9ad14d8e6095b65091ddef2d8fc2062bb151
Gürhan Küçük <gurhan.kucuk@outlook.com>
2023-02-04 22:29:57 +0300

    added licensing service

---
+++ licensing-service/src/main/java/com/optimagrowth/license/service/LicenseService.java
@@ -0,0 +1,103 @@
+package com.optimagrowth.license.service;
+
+import com.optimagrowth.license.config.ServiceConfig;
+import com.optimagrowth.license.model.License;
+import com.optimagrowth.license.model.Organization;
+import com.optimagrowth.license.repository.LicenseRepository;
+import com.optimagrowth.license.service.client.OrganizationRestTemplateClient;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.concurrent.TimeoutException;
+
+@Service
+public class LicenseService {
+
+    @Autowired
+    MessageSource messages;
+
+    @Autowired
+    private LicenseRepository licenseRepository;
+
+    @Autowired
+    private OrganizationRestTemplateClient organizationRestTemplateClient;
+
+    @Autowired
+    ServiceConfig config;
+
+    public License getLicense(String licenseId, String organizationId){
+        License license = licenseRepository.findByOrganizationIdAndLicenseId(organizationId, licenseId);
+        if (null == license) {
+            throw new IllegalArgumentException(String.format(messages.getMessage("license.search.error.message", null, null),licenseId, organizationId));
+        }
+
+        Organization organization  = organizationRestTemplateClient.getOrganization(organizationId);
+        if (null != organization) {
+            license.setOrganizationName(organization.getName());
+            license.setContactName(organization.getContactName());
+            license.setContactEmail(organization.getContactEmail());
+            license.setContactPhone(organization.getContactPhone());
+        }
+
+        return license.withComment(config.getProperty());
+    }
+
+    public License createLicense(License license){
+        license.setLicenseId(UUID.randomUUID().toString());
+        licenseRepository.save(license);
+
+        return license.withComment(config.getProperty());
+    }
+
+    public License updateLicense(License license){
+        licenseRepository.save(license);
+
+        return license.withComment(config.getProperty());
+    }
+
+    public String deleteLicense(String licenseId){
+        String responseMessage = null;
+        License license = new License();
+        license.setLicenseId(licenseId);
+        licenseRepository.delete(license);
+        responseMessage = String.format(messages.getMessage("license.delete.message", null, null),licenseId);
+        return responseMessage;
+
+    }
+
+    @CircuitBreaker(name = "licenceService", fallbackMethod = "buildFallbackLicenseList")
+    public List<License> getLicensesByOrganizationId(String organizationId) throws TimeoutException {
+        randomlyRunLong();
+        return licenseRepository.findByOrganizationId(organizationId);
+    }
+
+    private void randomlyRunLong() throws TimeoutException {
+        Random rand = new Random();
+        int randomNum = rand.nextInt(3) + 1;
+        if(randomNum == 3) {
+            sleep();
+        }
+    }
+
+    private void sleep() throws TimeoutException {
+        try {
+            Thread.sleep(5000);
+            throw new java.util.concurrent.TimeoutException();
+        }catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private List<License> buildFallbackLicenseList(String organizationId, Throwable t){
+        List<License> fallbackList = new ArrayList<>();
+        License license = new License();
+        license.setLicenseId("0000000-00-00000");
+        license.setOrganizationId(organizationId);
+        license.setProductName("Sorry no licensing information currently available");
+        fallbackList.add(license);
+        return fallbackList;
+    }
+}
